d743bc94963a61032dcfd75423ca913c0c78e842
Kaue <kauelive24@gmail.com>
2025-07-29 18:27:32 -0300

    menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'seven-app';
+export class AppComponent implements OnInit {
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            { label: 'Jovens', icon: 'pi pi-user' },
+            { label: 'Departamento', icon: 'pi pi-briefcase' },
+            { label: 'Participação', icon: 'pi pi-pen-to-square' },
+            { label: 'Presença', icon: 'pi pi-calendar' },
+        ];
+    }
 }
