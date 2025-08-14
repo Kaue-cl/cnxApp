d263f02716a69766e13e38838d4c4f2afe16f9df
Kaue <kauelive24@gmail.com>
2025-08-14 18:22:25 -0300

    rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,17 @@ export const routes: Routes = [
         path: 'jovens',
         loadComponent: () => import('./page/jovens/jovens.component').then(m => m.JovensComponent)
     },
+    {
+        path: 'departamento',
+        loadComponent: () => import('./page/departamento/departamento.component').then(m => m.DepartamentoComponent)
+    },
+    {
+        path: 'participacao',
+        loadComponent: () => import('./page/participacao/participacao.component').then(m => m.ParticipacaoComponent)
+    },
+    {
+        path: 'presenca',
+        loadComponent: () => import('./page/presenca/presenca.component').then(m => m.PresencaComponent)
+    }
 
 ];
